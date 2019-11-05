42aadf378da7fce07eb1e0044ec2f271a76a89c3
Leon Vinogradov <win.leon@protonmail.com>
2019-11-05 23:15:23 +0300

    chore: move default options to separate file

--- src/index.js
+++ src/index.js
@@ -3,29 +3,7 @@ const { JWK, JWT, JWE } = require('jose')
 
 const MemoryStorage = require('./storage/MemoryStorage')
 const RedisStorage = require('./storage/RedisStorage')
-
-const DEFAULT_OPTIONS = {
-  cookieOptions: {
-    domain: 'localhost',
-    httpOnly: true,
-    path: '/',
-    sameSite: 'strict',
-    secure: false
-  },
-
-  randomBytesSize: 64,
-
-  signSecret: 'SIGN_SECRET',
-  encodeSecret: 'ENCODE_SECRET',
-
-  accessTokenName: 'ACCESS_TOKEN_NAME',
-  refreshTokenName: 'REFRESH_TOKEN_NAME',
-  csrfTokenName: 'CSRF_TOKEN_NAME',
-
-  accessTokenMaxAge: 5 * 60 * 1000, // 5 minutes in ms
-  refreshTokenMaxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
-  csrfTokenMaxAge: 7 * 24 * 60 * 60 * 1000 // 7 days in ms
-}
+const DEFAULT_OPTIONS = require('./defaultOptions')
 
 const getRandomToken = (size) => {
   return crypto
